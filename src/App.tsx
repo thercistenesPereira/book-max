3924fb79f37d078b646c5ea46f2b076769b9ff3b
thercistenes <thercistenes1@outlook.com>
2024-07-13 10:10:39 -0300

    cria botão lista de desejo

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,41 @@
+import { useState } from 'react';
 import './App.css';
 import Book from './components/Book';
 import BookList from './components/BookList';
 import data from './data.json';
-
-const bookIndexStart = 10;
-const bookIndexEnd = 15;
-const placeHolderList = data.slice(bookIndexStart, bookIndexEnd); // esse código deverá ser excluído após a implementação do requisito 2
+import { BookInfoType } from './types';
 
 function App() {
+  const [currentBook, setCurrentBook] = useState(0);
+  const [wishList, setWishList] = useState<BookInfoType[]>([]);
+  const [readingList, setReadingList] = useState<BookInfoType[]>([]);
+  const [readList, setReadList] = useState<BookInfoType[]>([]);
+
   return (
     <div className="app">
       <div className="book-selector">
-        <Book bookInfo={ data[0] } showDetails />
+        <Book bookInfo={ data[currentBook] } showDetails />
         <div className="selector-buttons">
-          <button>Adicionar à lista de desejos</button>
-          <button>Adicionar à lista de leitura</button>
-          <button>Adicionar à lista de lidos</button>
-          <button>Próximo livro</button>
+          <button
+            onClick={ () => setWishList([...wishList, data[currentBook]]) }
+          >
+            Adicionar à lista de desejos
+          </button>
+          <button
+            onClick={ () => setReadingList([...readingList, data[currentBook]]) }
+          >
+            Adicionar à lista de leitura
+          </button>
+          <button
+            onClick={ () => setReadList([...readList, data[currentBook]]) }
+          >
+            Adicionar à lista de lidos
+          </button>
+          <button
+            onClick={ () => setCurrentBook((currentBook + 1) % data.length) }
+          >
+            Próximo livro
+          </button>
         </div>
       </div>
 
@@ -26,7 +45,7 @@ function App() {
         <button>Exibir lista de lidos</button>
       </div>
       <h1>Lista de ...</h1>
-      <BookList books={ placeHolderList } />
+      <BookList books={ wishList } />
     </div>
   );
 }
